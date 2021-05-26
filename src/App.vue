8a12b8672491ff7023d57e0b2da8469b39b43da7
Günhan Selaş <hsnglsn@gmail.com>
2021-05-26 19:52:55 +0300

    some styles and categories

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <span @click="fetch">Hasan</span>
-      {{results}}
+  <div id="app" class="bg-gray-100 flex justify-center items-center h-screen">
+    <div class="max-w-2xl mx-auto relative bg-gray-50 shadow-xl p-5 rounded-sm">
+      <nav class="flex justify-end items-center space-x-1 sticky top-0 py-5 z-10 ">
+        <button>Home</button>
+        <button>
+          <span>Categories</span>
+          <ul>
+            <li ></li>
+          </ul>
+        </button>
+        <button>Random</button>
+      </nav>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import recipeService from '@/services/recipeService'
-import {AxiosResponse} from 'axios'
 export default Vue.extend({
   data() {
     return {
-      results: {} as AxiosResponse
+      recipes: []
     }
   },
   methods: {
-    async fetch () {
-      await recipeService.complexSearch({
-        query: 'egg',
-        cuisine: 'european'
-      }).then((resp) => {
-        this.results = resp.data
-      }).catch( err => {
-        return err
-      })
-    }
   }
 })
 </script>
 <style>
-
+nav > button {
+  @apply text-gray-100 text-sm font-semibold tracking-tight bg-indigo-500 rounded-sm px-2 py-1 hover:bg-indigo-700;
+}
 </style>
